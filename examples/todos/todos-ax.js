e6b4ee07e7e33efffbac6c0cc134432aede3dd9f
vlad <iamvlad@gmail.com>
2012-03-17 11:58:13 -0700

    speak new todo placeholder in chrome

---
+++ examples/todos/todos-ax.js
@@ -0,0 +1,39 @@
+var TodoAx = function() {
+};
+
+_.extend(TodoAx.prototype, {
+  placeholders: function() {
+    var $newTodoField = $('#new-todo');
+    this.label($newTodoField, $newTodoField.attr('placeholder'));
+  },
+
+  label: function($elem, labelText, labelId) {
+    $elem.$label = $('<label>', {
+      id: labelId || 'AX-lbl--' + $elem.attr('id'),
+      text: $.isFunction(labelText) ? labelText($elem) : labelText,
+      'aria-role': 'label',
+    });
+    this.getLabelsContainer().append($elem.$label);
+
+    $elem.attr('aria-labelledby', $elem.$label.attr('id'));
+  },
+
+  getLabelsContainer: function() {
+    if (!this.$labelsContainer) {
+      this.$labelsContainer = $('<div>', {
+        id: 'AX-container--labels',
+        css: {
+          display: 'none'
+        }
+      });
+      $('body').append(this.$labelsContainer);
+    }
+    return this.$labelsContainer;
+  }
+
+});
+
+$(function() {
+  var todoAx = new TodoAx;
+  todoAx.placeholders();
+});
